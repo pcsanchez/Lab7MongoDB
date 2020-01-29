2201bcd220dc1fd4774be8af06dc57fd2d9c720a
Paco <paco.sanchez.ram@gmail.com>
2020-01-29 16:46:50 -0600

    fixed db link

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://admin:admin@cluster0-7kuwl.mongodb.net/blog?retryWrites=true&w=majority";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blog";
 exports.PORT = process.env.PORT || 8080;
